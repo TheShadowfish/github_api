727f386b872487530fb8e9b697b36f508c2b2a26
TheShadowfish <shadowfish@yandex.ru>
2024-04-22 13:17:56 +0300

    half-work

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 from src.functions import get_repos_stats
+from src.postgres_db import PostgresDB
 
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+db_config = {
+    'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('POSTGRES_PASSWORD'),
+    'host': os.getenv('POSTGRES_HOST'),
+    'port': os.getenv('POSTGRES_PORT'),
+    'dbname': os.getenv('POSTGRES_DB')
+}
+
+
+def main():
+    data = get_repos_stats('skypro-008')
+    db = PostgresDB(**db_config)
+    db.insert_data(data)
+
+    for item in db.get_data(5, 'forks'):
+        print(item)
+
+    db.export_to_json()
+
+
+if __name__ == '__main__':
+    main()
 
 def entry_point(name: str):
     # Use a breakpoint in the code line below to debug your script.
